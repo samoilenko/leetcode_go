b0ce116541930c5951c361fe24f9344706eac34b
Sergey Samoilenko <sergey.samoilenko@dev-pro.net>
2019-01-29 13:55:37 +0200

    1, 2 tasks

---
+++ AddTwoNumbers/main.go
@@ -0,0 +1,65 @@
+package main
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	return calc(l1, l2)
+}
+
+func calc(l1 *ListNode, l2 *ListNode) *ListNode{
+	var res *ListNode
+
+	extra := 0;
+	res = &ListNode{}
+	node := res
+	for{
+		n := l1.Val + l2.Val + extra;
+
+		if n < 10 {
+			node.Val = n
+			extra = 0
+		} else {
+			node.Val = n % 10
+			extra = n / 10
+		}
+
+		if l1.Next == nil && l2.Next == nil {
+			if extra > 0 {
+				node.Next = &ListNode{Val: extra}
+			}
+			return res
+		}
+
+		node.Next = &ListNode{}
+		node = node.Next
+
+		if l1.Next == nil {
+			l1.Val = 0
+		}else {
+			l1 = l1.Next
+		}
+
+		if l2.Next == nil {
+			l2.Val = 0
+		}else {
+			l2 = l2.Next
+		}
+	}
+}
+
+func main() {
+	//[1]
+	//[9,9]
+	//
+	//fmt.Println(addTwoNumbers());
+}
---
+++ TwoSum/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+// func twoSum(nums []int, target int) []int {
+// 	var res []int
+// 	for i := 0; i < len(nums); i++ {
+// 		for j := i+1; j < len(nums); j++ {
+// 			if nums[i]+nums[j] == target {
+// 				return append(res, i, j)
+// 			}
+// 		}
+
+// 	}
+
+// 	return res
+// }
+
+func twoSum(nums []int, target int) []int {
+	copy := make(map[int]int)
+	for i:=0 ;i<len(nums);i++{
+		nmb := target - nums[i];
+
+		if _, ok := copy[nmb]; ok {
+			return []int{copy[nmb], i}
+		}
+
+		copy[nums[i]] = i;
+	}
+
+	return []int{}
+}
+
+func main() {
+	fmt.Prinyln(twoSum([]int{2,7,11,15}, 9))
+}
