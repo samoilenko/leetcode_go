96f3eb2b1f213febc88ef15a0095e97dd8ff9158
Sergey Samoilenko <sergey.samoilenko@dev-pro.net>
2019-01-31 18:58:02 +0200

    #3

--- LongestSubstringWithoutRepeatingCharacters/main.go
+++ LongestSubstringWithoutRepeatingCharacters/main.go
@@ -1,40 +1,21 @@
 package main
 
 import (
-"fmt"
+	"fmt"
 )
 
 func main() {
 	//s:= "ad日本語v日dfg"
-	s:= "pwwkew"
-	//maxLength:=0
-	//
-	//current := make(map[rune]int)
-	//
-	//for i, c:= range s {
-	//	if curIndex, ok := current[c]; ok {
-	//		if len(current) > maxLength {
-	//			maxLength = len(current)
-	//		}
-	//		current = make(map[rune]int)
-	//		for ni, k := range s[curIndex: i] {
-	//			current[k] = ni
-	//		}
-	//		//for j := curIndex; j < i; {
-	//		//
-	//		//}
-	//	} else {
-	//		current[c] = i
-	//	}
-	//}
+	s:= "yfsrsrpzuya"
+	//s:= "dvdf"
 
 	fmt.Println(lengthOfLongestSubstring(s))
 }
 
-
 func lengthOfLongestSubstring(s string) int {
 	maxLength := 0
 	current := make(map[rune]int)
+	lastIndex := 0
 
 
 	for i, c:= range s {
@@ -42,16 +23,23 @@ func lengthOfLongestSubstring(s string) int {
 			if len(current) > maxLength {
 				maxLength = len(current)
 			}
-			current = make(map[rune]int)
-			for ni, k := range s[curIndex: i] {
-				current[k] = ni
+			//current = make(map[rune]int)
+
+			//for _, k := range s[curIndex: i] {
+			for _, k := range s[lastIndex:curIndex] {
+				delete(current, k)
+				//current[k] = ni
 			}
+
+			lastIndex = curIndex
+
+			//delete(current, c)
 			//for j := curIndex; j < i; {
 			//
 			//}
-		} else {
-			current[c] = i
 		}
+
+		current[c] = i
 	}
 
 	if len(current) > maxLength {
